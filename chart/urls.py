7b3738889484b21963800ed3c32631993263a00f
Chunar <m18742021187@163.com>
2020-07-02 20:01:12 +0800

    add chart pages

--- chart/urls.py
+++ chart/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('line/', views.ChartView.as_view(), name='demo'),
-	path('lineUpdate/', views.ChartUpdateView.as_view(), name='demo'),
-	path('index/', views.IndexView.as_view(), name='demo'),
+	path('currentchart/', views.CurrentChart.as_view(), name='chart'),
+	path('voltagechart/', views.VoltageChart.as_view(), name='chart'),
+	path('temperaturechart/', views.TemperatureChart.as_view(), name='chart'),
+	path('lineUpdate/', views.ChartUpdateView.as_view(), name='chart'),
+	path('index/', views.IndexView.as_view(), name='chart'),
+	path('current/', views.CurrentView.as_view(), name='chart'),
+	path('voltage/', views.VoltageView.as_view(), name='chart'),
+	path('temperature/', views.TemperatureView.as_view(), name='chart'),
 ]
 
--- chart/views.py
+++ chart/views.py
@@ -43,7 +43,29 @@ JsonResponse = json_response
 JsonError = json_error
 
 
-def line_base() -> Line:
+def current() -> Line:
+    line = (
+        Line()
+        .add_xaxis(list(range(10)))
+        .add_yaxis(series_name="A相电流", 
+				   y_axis=[randrange(0, 100) for _ in range(10)],
+				   is_smooth=True,)
+        .add_yaxis(series_name="B相电流", 
+				   y_axis=[randrange(0, 100) for _ in range(10)],
+				   is_smooth=True,)
+        .add_yaxis(series_name="C相电流", 
+				   y_axis=[randrange(0, 100) for _ in range(10)],
+				   is_smooth=True,)
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="电流"),
+            xaxis_opts=opts.AxisOpts(type_="value"),
+            yaxis_opts=opts.AxisOpts(type_="value")
+        )
+        .dump_options_with_quotes()
+    )
+    return line
+
+def voltage() -> Line:
     line = (
         Line()
         .add_xaxis(list(range(10)))
@@ -51,7 +73,23 @@ def line_base() -> Line:
 				   y_axis=[randrange(0, 100) for _ in range(10)],
 				   is_smooth=True,)
         .set_global_opts(
-            title_opts=opts.TitleOpts(title="动态数据"),
+            title_opts=opts.TitleOpts(title="电压"),
+            xaxis_opts=opts.AxisOpts(type_="value"),
+            yaxis_opts=opts.AxisOpts(type_="value")
+        )
+        .dump_options_with_quotes()
+    )
+    return line
+
+def temperature() -> Line:
+    line = (
+        Line()
+        .add_xaxis(list(range(10)))
+        .add_yaxis(series_name="", 
+				   y_axis=[randrange(0, 100) for _ in range(10)],
+				   is_smooth=True,)
+        .set_global_opts(
+            title_opts=opts.TitleOpts(title="温度"),
             xaxis_opts=opts.AxisOpts(type_="value"),
             yaxis_opts=opts.AxisOpts(type_="value")
         )
@@ -60,9 +98,17 @@ def line_base() -> Line:
     return line
 
 
-class ChartView(APIView):
+class CurrentChart(APIView):
     def get(self, request, *args, **kwargs):
-        return JsonResponse(json.loads(line_base()))
+        return JsonResponse(json.loads(current()))
+    
+class VoltageChart(APIView):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse(json.loads(voltage()))
+
+class TemperatureChart(APIView):
+    def get(self, request, *args, **kwargs):
+        return JsonResponse(json.loads(temperature()))
 
 
 cnt = 9
@@ -78,3 +124,15 @@ class IndexView(APIView):
     def get(self, request, *args, **kwargs):
         # return HttpResponse(content=open("./templates/index.html").read())
         return render(request, 'chart/index.html')
+
+class CurrentView(APIView):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'chart/current.html')
+
+class VoltageView(APIView):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'chart/voltage.html')
+
+class TemperatureView(APIView):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'chart/temperature.html')
